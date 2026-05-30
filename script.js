b54ef0225f2bea00ee6919a35eec601e0a56ef3a
Mark Mote <137040517+markmote1@users.noreply.github.com>
2026-05-30 10:23:03 +0300

    Styling updates

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 
 
-let socials = document.querySelector('#socials')
+window.onload = ()=>{
+    console.log('init')
+    let socials = document.querySelector('#socials')
+let canvas = document.querySelector('#canvas1')
+
+canvas.height = 500
+canvas.width = 200
+
+let ctx = canvas.getContext('2d')
+ctx.beginPath()
+ctx.fillStyle = 'white'
+ctx.arc(10,20,4,0,360)
+
+ctx.fill()
 
 document.addEventListener('scroll',(e)=>{
     console.log(e)
@@ -17,3 +30,4 @@ document.addEventListener('scrollend',(e)=>{
 
 //add a click me b4 i disa]ear button on canvas maybe to visit sth like github
 
+}
