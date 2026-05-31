b261090c4cafbdd113d36923e00981b824af2811
Mark Mote <137040517+markmote1@users.noreply.github.com>
2026-05-31 04:37:31 +0300

    Added function to add projects dynamically to the my projects from projects stored in a module

---
+++ projects.js
@@ -0,0 +1,172 @@
+let projects = [
+    {
+        id: 1,
+        title: 'Web app 1',
+        description: 'A fullstack web app',
+        image: '/images/markmote.jpg',
+        screenshots: [{img1: 'img',
+            about: 'A screenshot showing hero section'
+
+
+        },{
+            img2: 'img',
+            about: 'A screenshot showing about section'
+
+        },{
+            img3: 'img',
+            about: 'A screenshot showing dashboard section'
+
+        }]
+    },
+
+    {
+        id: 2,
+        title: 'Web app 2',
+        description: 'A blog web app',
+        image: '/images/markmote.jpg',
+        screenshots: [{img1: 'img',
+            about: 'A screenshot showing hero section'
+
+
+        },{
+            img2: 'img',
+            about: 'A screenshot showing about section'
+
+        },{
+            img3: 'img',
+            about: 'A screenshot showing dashboard section'
+
+        }]
+    },
+
+    {
+        id: 3,
+        title: 'Web app 3',
+        description: 'An ecommerce web app',
+        image: '/images/markmote.jpg',
+        screenshots: [{img1: 'img',
+            about: 'A screenshot showing hero section'
+
+
+        },{
+            img2: 'img',
+            about: 'A screenshot showing about section'
+
+        },{
+            img3: 'img',
+            about: 'A screenshot showing dashboard section'
+
+        }]
+    },
+
+    {
+        id: 4,
+        title: 'Web app 4',
+        description: 'A gym web app',
+        image: '/images/markmote.jpg',
+        screenshots: [{img1: 'img',
+            about: 'A screenshot showing hero section'
+
+
+        },{
+            img2: 'img',
+            about: 'A screenshot showing about section'
+
+        },{
+            img3: 'img',
+            about: 'A screenshot showing code section'
+
+        }]
+    },
+
+    {
+        id: 5,
+        title: 'Web app 5',
+        description: 'A pharmacy web app',
+        image: '/images/markmote.jpg',
+        screenshots: [{img1: 'img',
+            about: 'A screenshot showing hero section'
+
+
+        },{
+            img2: 'img',
+            about: 'A screenshot showing styling section'
+
+        },{
+            img3: 'img',
+            about: 'A screenshot showing about section'
+
+        }]
+    },
+
+    {
+        id: 6,
+        title: 'Web app 6',
+        description: 'A construction web app',
+        image: '/images/markmote.jpg',
+        screenshots: [{img1: 'img',
+            about: 'A screenshot showing hero section'
+
+
+        },{
+            img2: 'img',
+            about: 'A screenshot showing about section'
+
+        },{
+            img3: 'img',
+            about: 'A screenshot showing contact section'
+
+        }]
+    },
+
+    {
+        id: 7,
+        title: 'Web app 7',
+        description: 'A school web app',
+        image: '/images/markmote.jpg',
+        screenshots: [{img1: 'img',
+            about: 'A screenshot showing hero section'
+
+
+        },{
+            img2: 'img',
+            about: 'A screenshot showing about section'
+
+        },{
+            img3: 'img',
+            about: 'A screenshot showing usage section'
+
+        }]
+    },
+
+    {
+        id: 8,
+        title: 'Web app 8',
+        description: 'A market web app',
+        image: '/images/markmote.jpg',
+        screenshots: [{img1: 'img',
+            about: 'A screenshot showing hero section'
+
+
+        },{
+            img2: 'img',
+            about: 'A screenshot showing about section'
+
+        },{
+            img3: 'img',
+            about: 'A screenshot showing why section'
+
+        }]
+    },
+]
+
+
+// let img = (project)=>{
+//     let image = 
+//     project.image = image
+// }
+// projects.forEach(img)
+
+export {projects}
+
+
--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 
 
+import {projects} from "./projects.js"
 window.onload = ()=>{
-    console.log('init')
-    let socials = document.querySelector('#socials')
+
+console.log('init')
+
+let socials = document.querySelector('#socials')
+let projectHolder = document.querySelector('#allProjects')
 let canvas = document.querySelector('#canvas1')
 
+
+let createCard = (project)=>{
+    let titleHolder = document.createElement('h3')
+    let descriptionHolder = document.createElement('p')
+    let imageHolder = document.createElement('img')
+    imageHolder.classList.add('cardImage')
+    let card = document.createElement('li')
+
+    titleHolder.textContent = project.title
+    imageHolder.src = project.image
+    descriptionHolder.textContent = project.description
+    
+    card.append(titleHolder,imageHolder, descriptionHolder)
+    card.classList.add('projectWrapper')
+    projectHolder.append(card)
+
+
+    
+   
+}
+
+projects.forEach(createCard)
+
+
+console.log(projects)
 canvas.height = 490
 canvas.width = 200
 
